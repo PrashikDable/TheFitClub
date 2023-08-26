a40e2e0a9c28260c108e1340b571b4849f31d391
PrashikDable <125259111+PrashikDable@users.noreply.github.com>
2023-08-26 09:26:42 +0530

    Delete TheFitClub directory

--- TheFitClub/src/main/java/com/Project/TheFitClub/service/trainerService.java
+++
@@ -1,9 +0,0 @@
-package com.Project.TheFitClub.service;
-
-import com.Project.TheFitClub.model.trainer;
-
-public interface trainerService {
-
-	void addPerson(trainer t);
-
-}
--- TheFitClub/src/main/java/com/Project/TheFitClub/service/usersService.java
+++
@@ -1,9 +0,0 @@
-package com.Project.TheFitClub.service;
-
-import com.Project.TheFitClub.model.users;
-
-public interface usersService {
-
-	void addUser(users u);
-
-}
